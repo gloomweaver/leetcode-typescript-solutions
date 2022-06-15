55d35ae19c08c2cc0fede86c976f5e3f9386e58a
Kirill Khramkov <hramkir@gmail.com>
2022-06-15 09:25:19 +0300

    Task 13 solved

---
+++ 13/solution.ts
@@ -0,0 +1,16 @@
+const dict: Record<string, number> = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+function romanToInt(s: string): number {
+  return s.split("").reduce((calc, cur, index, array) => {
+    if (dict[cur] < dict[array[index + 1]]) return calc - dict[cur];
+    return calc + dict[cur];
+  }, 0);
+}
